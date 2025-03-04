e3e4e859172fabcda2561bb467b8f798cc172cac
Shaurya Sood <115377200+SoodShaurya@users.noreply.github.com>
2025-03-04 00:08:43 -0800

    fix import

--- main/daita_manager/server.py
+++ main/daita_manager/server.py
@@ -4,11 +4,11 @@ import asyncio
 
 from config.__init__ import load_config
 
-from .scrapers.reddit_scraper import RedditScraper
-from .backend.models import SentimentAnalyzer
-from .scrapers.data_processor import DataProcessor
-from .backend.symbol_manager import SymbolManager
-from .backend.api import API
+from scrapers.reddit_scraper import RedditScraper
+from backend.models import SentimentAnalyzer
+from scrapers.data_processor import DataProcessor
+from backend.symbol_manager import SymbolManager
+from backend.api import API
 
 from utils.logger import setup_logger
 from utils.mongo import initialize_mongodb
